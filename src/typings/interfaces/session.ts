bf34033fcd1ebe96baf5d636f942238c2da49fef
ix1osbek <bekerkinov2004@gmail.com>
2025-08-19 12:59:19 +0500

    .

--- src/typings/interfaces/session.ts
+++ src/typings/interfaces/session.ts
@@ -2,5 +2,5 @@ import { ScenesSessionData } from "grammy-scenes";
 import { IUser } from "../../models/user.model";
 
 export interface SessionData extends ScenesSessionData {
-  user: IUser;
+  user: IUser
 }
